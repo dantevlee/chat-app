3868973fbb7c4deb25e985d89d0e17400997d653
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2023-10-12 20:22:32 -0500

    fixed db

--- api/database/chat.sql
+++ api/database/chat.sql
@@ -1,18 +1,17 @@
+-- CREATE DATABASE chat_app;
+
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     firstName VARCHAR(225) NOT NULL,
     lastName VARCHAR(225) NOT NULL,
     email VARCHAR(225) NOT NULL,
-    password VARCHAR(225) NOT NULL
-    
+    password VARCHAR(225) NOT NULL,
+    last_active_at timestamp without time zone;
     );
 
-ALTER TABLE users
-ADD COLUMN last_active_at timestamp without time zone;
-
-CREATE TABLE messages(
-     message_id SERIAL PRIMARY KEY,
-	 message_text VARCHAR(225) NOT NULL,
-     created_at TIMESTAMP NOT NULL,
-     user_id int REFERENCES users(id) NOT NULL
-    );
+-- CREATE TABLE messages(
+--      message_id SERIAL PRIMARY KEY,
+-- 	 message_text VARCHAR(225) NOT NULL,
+--      created_at TIMESTAMP NOT NULL,
+--      user_id int REFERENCES users(id) NOT NULL
+--     );
