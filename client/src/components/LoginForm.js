3145ee2abefb1b10233313e0859572202396cac9
dantevlee <dante.leeseberg@gmail.com>
2024-02-06 11:49:31 -0600

    render fragment banner

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState, Fragment } from "react";
 import axios from "axios";
-import { Alert } from 'react-bootstrap';
+import { Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import chat from "../assets/chat.png";
@@ -9,14 +9,14 @@ const LoginForm = ({ setIsLoggedIn }) => {
   let emailInputRef = useRef();
   let passwordInputRef = useRef();
   const [error, setError] = useState("");
-  const [visitCount, setVisitCount] = useState(1)
+  const [visitCount, setVisitCount] = useState(1);
   const [isInitialLoad, setIsInitialLoad] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (visitCount === 1){
-      setIsInitialLoad(true)
+    if (visitCount === 1) {
+      setIsInitialLoad(true);
     }
 
     const loginBody = {
@@ -25,101 +25,109 @@ const LoginForm = ({ setIsLoggedIn }) => {
     };
 
     try {
-      const response = await axios.post("https://chat-rest.onrender.com/api/login", loginBody);
+      const response = await axios.post(
+        "https://chat-rest.onrender.com/api/login",
+        loginBody
+      );
 
       if (response.status === 400) {
-        setError('User not found or password is incorrect.');
-        setIsInitialLoad(false)
+        setError("User not found or password is incorrect.");
+        setIsInitialLoad(false);
         return;
       }
-  
+
       const user = response.data.token;
-  
+
       localStorage.setItem("token", user);
-  
+
       localStorage.setItem("email", response.data.email);
-  
+
       if (response.status === 200) {
         setIsLoggedIn(true);
       }
 
-      setIsInitialLoad(false)
-      if (visitCount > 0){
-        setVisitCount(0)
+      setIsInitialLoad(false);
+      if (visitCount > 0) {
+        setVisitCount(0);
       }
-
-    } catch(err) {
-      setError('An error occurred please try again later.')
+    } catch (err) {
+      setError("An error occurred please try again later.");
     }
-
-   
   };
 
   const removeNotification = () => {
     setIsInitialLoad(false);
-    setVisitCount(0)
-  }
+    setVisitCount(0);
+  };
 
   return (
     <Fragment>
-    <div className="container d-flex justify-content-center align-items-center vh-100">
-      <form
-        style={{
-          width: 400,
-          border: "1px solid",
-          backgroundColor: "#cad5df",
-          borderRadius: "5mm",
-          padding: "20px",
-        }}
-        onSubmit={handleSubmit}
-      >
-        <img
-          src={chat}
-          alt="chit-chit"
+      {isInitialLoad && (
+        <Alert variant="info" onClose={() => removeNotification()} dismissible>
+          This server is running on a free instance in the cloud that spins down
+          if unused. It may take a few seconds for the first app log-in attempt.
+          Thank you for your patience!
+        </Alert>
+      )}
+
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <form
           style={{
-            maxWidth: "100%",
-            height: "auto",
+            width: 400,
+            border: "1px solid",
+            backgroundColor: "#cad5df",
             borderRadius: "5mm",
-            marginBottom: "20px",
+            padding: "20px",
           }}
-        />
-         {error && <div className="alert alert-danger">{error}</div>}
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email Address
-          </label>
-          <input
-            required
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="Email..."
-            ref={emailInputRef}
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={chat}
+            alt="chit-chit"
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+              borderRadius: "5mm",
+              marginBottom: "20px",
+            }}
           />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            required
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password..."
-            ref={passwordInputRef}
-          />
-        </div>
+          {error && <div className="alert alert-danger">{error}</div>}
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email Address
+            </label>
+            <input
+              required
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Email..."
+              ref={emailInputRef}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              required
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password..."
+              ref={passwordInputRef}
+            />
+          </div>
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
 
-        <p className="mt-3">
-          Don't have an account? <Link to="/register">REGISTER</Link>
-        </p>
-      </form>
-    </div>
+          <p className="mt-3">
+            Don't have an account? <Link to="/register">REGISTER</Link>
+          </p>
+        </form>
+      </div>
     </Fragment>
   );
 };
