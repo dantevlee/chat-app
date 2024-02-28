84daae63b22955c22c05db4c1a736352e29c5907
dantevlee <dante.leeseberg@gmail.com>
2024-02-27 21:51:13 -0600

    channels issue

--- api/routes/channels.js
+++ api/routes/channels.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 router.get('/channels', async(req, res) => {
   try {
     const token = req.headers.token;
-    const chanels = await db.query(`SELECT channel FROM channels ORDER BY ID`);
+    const channels = await db.query(`SELECT channel FROM channels ORDER BY ID`);
 
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
   
@@ -16,7 +16,7 @@ router.get('/channels', async(req, res) => {
       return res.status(401).json('Unauthorized');
     }
 
-    res.json(messages.rows);
+    res.json(channels.rows);
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message);
